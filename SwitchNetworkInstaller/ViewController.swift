0d4dcf2ff9f61fa2feae386a6b2b93531e4239dd
Dylan Elliott <dylan.elliott@gmail.com>
2019-01-01 15:30:41 +1100

    Add log view

--- SwitchNetworkInstaller/ViewController.swift
+++ SwitchNetworkInstaller/ViewController.swift
@@ -10,47 +10,40 @@ import Cocoa
 
 class ViewController: NSViewController, NSControlTextEditingDelegate {
 	
+	
 	@IBOutlet var filePathLabel: NSTextField!
 	@IBOutlet var switchIPAddressField: NSTextField!
 	@IBOutlet var installButton: NSButton!
+	@IBOutlet var logView: NSTextView!
 	
 	var selectedFileURL: URL?
+	var logLineStrings: [String] = []
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		// Do any additional setup after loading the view.
-		updateView()
+		
+		updateInstallButton()
 	}
 
-	override var representedObject: Any? {
-		didSet {
-		// Update the view, if already loaded.
-		}
-	}
-	
 	@IBAction func fileSelectButtonTapped(sender: NSButton) {
 		let panel = NSOpenPanel()
 		panel.allowedFileTypes = ["nsp"]
 		panel.begin { response in
 			if response == NSApplication.ModalResponse.OK {
 				self.selectedFileURL = panel.url
-				self.updateView()
+				self.updateInstallButton()
 			}
 		}
 	}
 	
-	@IBAction func installButtonTapped(sender: NSButton) {
-		let escapedURLString = (selectedFileURL! as NSURL).resourceSpecifier!
-		let urlString = escapedURLString.removingPercentEncoding!
-		runNetworkInstall(filePath: urlString, ipAddress: switchIPAddressField.stringValue)
+	func controlTextDidChange(_ obj: Notification) {
+		updateInstallButton()
 	}
 	
-	func updateView() {
-		filePathLabel.stringValue = selectedFileURL?.absoluteString ?? "No file selected..."
-
-		let fileSelected = (selectedFileURL != nil)
+	private func updateInstallButton() {
+		filePathLabel.stringValue = (selectedFileURL as NSURL?)?.resourceSpecifier?.removingPercentEncoding ?? "No file selected..."
 		
+		let fileSelected = (selectedFileURL != nil)
 		
 		let ipAddressString = switchIPAddressField.stringValue
 		let ipAddressRegex = try! NSRegularExpression(pattern: "\\d+\\.\\d+\\.\\d+\\.\\d+")
@@ -62,27 +55,53 @@ class ViewController: NSViewController, NSControlTextEditingDelegate {
 		
 		installButton.isEnabled = (fileSelected && validIPAddressEntered)
 	}
-
-	func controlTextDidChange(_ obj: Notification) {
-		updateView()
-	}
-
-}
-
-extension ViewController {
-	@discardableResult
-	func shell(launchPath: String, args: String...) -> Int32 {
-		let task = Process()
-		task.launchPath = launchPath
-		task.arguments = args
-		task.launch()
-		task.waitUntilExit()
-		return task.terminationStatus
+	
+	@IBAction func installButtonTapped(sender: NSButton) {
+		logLineStrings.removeAll()
+		updateLogTextView()
+		
+		let urlString = (selectedFileURL! as NSURL).resourceSpecifier!.removingPercentEncoding!
+		runNetworkInstall(filePath: urlString, ipAddress: switchIPAddressField.stringValue)
 	}
 	
 	func runNetworkInstall(filePath: String, ipAddress: String) {
 		print(filePath, ipAddress)
 		let scriptPath = Bundle.main.path(forResource: "remote_install_pc", ofType: "py")!
-		shell(launchPath: "/usr/local/bin/python3", args: scriptPath, ipAddress, filePath)
+		shell(launchPath: "/usr/local/bin/python3", args: scriptPath, ipAddress, filePath, onOutput: {
+			self.logLineStrings.append($0)
+			self.updateLogTextView()
+		})
+	}
+	
+	private func updateLogTextView() {
+		logView.string = logLineStrings.joined(separator: "\n")
+	}
+}
+
+extension ViewController {
+	func shell(launchPath: String, args: String..., onOutput: @escaping (String) -> Void) {
+		let task = Process()
+		task.launchPath = launchPath
+		task.arguments = args
+		
+		let pipe = Pipe()
+		task.standardOutput = pipe
+		task.standardError = pipe
+		let outHandle = pipe.fileHandleForReading
+		outHandle.waitForDataInBackgroundAndNotify()
+		
+		NotificationCenter.default.addObserver(
+			forName: NSNotification.Name.NSFileHandleDataAvailable,
+			object: outHandle, queue: nil) {  notification -> Void in
+				let data = outHandle.availableData
+				if data.count > 0 {
+					if let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+						onOutput(str as String)
+					}
+					outHandle.waitForDataInBackgroundAndNotify()
+				}
+		}
+		
+		task.launch()
 	}
 }
